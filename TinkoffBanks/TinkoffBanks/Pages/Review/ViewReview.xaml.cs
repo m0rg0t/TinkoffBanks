d68f6c791a04fbb9a6de9d69455f0cdef86bf13e
Anton Lenev <m0rg0t.Anton@gmail.com>
2013-07-06 02:38:48 +0400

    Twit view page

--- TinkoffBanks/TinkoffBanks/Pages/Review/ViewReview.xaml.cs
+++ TinkoffBanks/TinkoffBanks/Pages/Review/ViewReview.xaml.cs
@@ -7,6 +7,9 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Device.Location;
+using TinkoffBanks.ViewModel;
+using Microsoft.Phone.Controls.Maps;
 
 namespace TinkoffBanks.Pages.Review
 {
@@ -15,6 +18,29 @@ namespace TinkoffBanks.Pages.Review
         public ViewReview()
         {
             InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                GeoCoordinate currentLocation = new GeoCoordinate(ViewModelLocator.MainStatic.Latitued, ViewModelLocator.MainStatic.Longitude);
+                this.map1.Children.Add(new Pushpin() { Location = currentLocation, Content = "Я" });
+                
+                map1.Children.Add(new Pushpin() { 
+                    Location = ViewModelLocator.MainStatic.CurrentReview.GeoLocation, Content = ViewModelLocator.MainStatic.CurrentReview.Content.ToString() });
+                
+                map1.ZoomLevel = 14;
+                try
+                {
+                    map1.Center = ViewModelLocator.MainStatic.CurrentReview.GeoLocation;
+                }
+                catch {
+                    map1.Center = currentLocation;
+                };
+                
+            }
+            catch { };
         }
     }
 }
